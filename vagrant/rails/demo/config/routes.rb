39f318cfa958f54144677d4ef4a270355c003450
Nhan Nguyen <nathan13574@gmail.com>
2020-06-05 15:38:03 -0500

    demo

--- vagrant/rails/demo/config/routes.rb
+++ vagrant/rails/demo/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'products/index'
+
+  get 'products/new'
+
+  root 'products#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
