0711cf420b3513641d1442ffc483ff0522cd4553
Nhan Nguyen <nathan13574@gmail.com>
2020-06-07 04:00:16 -0500

    Dojo Dashboard II

--- vagrant/rails/rails_controller_view/dojoDashboard/app/controllers/dojo_controllers_controller.rb
+++ vagrant/rails/rails_controller_view/dojoDashboard/app/controllers/dojo_controllers_controller.rb
@@ -2,4 +2,19 @@ class DojoControllersController < ApplicationController
   def index
     @all_dojo = Dojo.all
   end
+  
+  def create
+    Dojo.create(
+      branch: params[:branch], 
+      street: params[:street],
+      city: params[:city],
+      state: params[:state]
+    )
+
+    redirect_to '/'
+  end
+
+  def new
+    render 'new'
+  end
 end
--- vagrant/rails/rails_controller_view/dojoDashboard/config/routes.rb
+++ vagrant/rails/rails_controller_view/dojoDashboard/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get '' => 'dojo_controllers#index'
+  get 'dojos/new' => 'dojo_controllers#new'
+  post 'create' => 'dojo_controllers#create'
 end
