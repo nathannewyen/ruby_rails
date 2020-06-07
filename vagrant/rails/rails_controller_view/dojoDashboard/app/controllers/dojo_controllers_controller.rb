e853286f21f86bd5f93e59a1c3a7f35d4c6fbc8e
Nathan Nguyen <nhan13574@gmail.com>
2020-06-07 17:38:04 -0500

    Dojo Dashboard

--- vagrant/rails/rails_controller_view/dojoDashboard/app/controllers/dojo_controllers_controller.rb
+++ vagrant/rails/rails_controller_view/dojoDashboard/app/controllers/dojo_controllers_controller.rb
@@ -1,16 +1,19 @@
 class DojoControllersController < ApplicationController
+  private
+    def dojo_params
+      params.require(:dojo).permit(:branch, :street, :city, :state)
+    end
+    
   def index
     @all_dojo = Dojo.all
   end
+
+  def new
+    render 'new'
+  end
   
   def create
-    Dojo.create(
-      branch: params[:branch], 
-      street: params[:street],
-      city: params[:city],
-      state: params[:state]
-    )
-
+    dojo = Dojo.create(dojo_params)
     redirect_to '/'
   end
 
@@ -18,7 +21,19 @@ class DojoControllersController < ApplicationController
     @dojo = Dojo.find(params[:id])
   end
 
-  def new
-    render 'new'
+  def edit 
+    @dojo = Dojo.find(params[:id])
+  end
+
+  def update
+    dojo = Dojo.find(params[:id])
+    dojo.update(dojo_params)
+    dojo.save
+    redirect_to "/dojos/#{dojo.id}"
+  end
+
+  def delete
+    Dojo.find(params[:id]).destroy
+    redirect_to '/'
   end
 end
--- vagrant/rails/rails_controller_view/dojoDashboard/config/routes.rb
+++ vagrant/rails/rails_controller_view/dojoDashboard/config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   get '' => 'dojo_controllers#index'
   get 'dojos/new' => 'dojo_controllers#new'
   get 'dojos/:id' => 'dojo_controllers#info'
+  get 'dojos/:id/edit' => 'dojo_controllers#edit'
   post 'create' => 'dojo_controllers#create'
+  patch 'dojos/:id/edit_process' => 'dojo_controllers#update'
+  delete 'dojos/:id/delete' => 'dojo_controllers#delete'
 end
