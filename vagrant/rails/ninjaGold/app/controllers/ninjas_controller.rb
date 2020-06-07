8602741aa93965fe1217b5b8831be0d73d264759
Nathan Nguyen <nhan13574@gmail.com>
2020-06-07 00:50:09 -0500

    updated ninja gold

--- vagrant/rails/ninjaGold/app/controllers/ninjas_controller.rb
+++ vagrant/rails/ninjaGold/app/controllers/ninjas_controller.rb
@@ -1,34 +1,30 @@
 class NinjasController < ApplicationController
   def index
     unless 
-      session[:gold]
       session[:gold] = 0
     end
 
     unless
-      session[:activities]
       session[:activities] = []
     end
-
     @activities = session[:activities]
-    puts @activities
   end
 
   def earngold
     if params[:location] == 'Farm'
-      new_gold = Random.rand(10..20)
+      new_gold = rand(10..20)
       session[:gold] += new_gold
     elsif
       params[:location] == 'Cave'
-      new_gold = Random.rand(5..10)
+      new_gold = rand(5..10)
       session[:gold] += new_gold
     elsif
       params[:location] == 'House'
-      new_gold = Random.rand(2..5)
+      new_gold = rand(2..5)
       session[:gold] += new_gold
     elsif
       params[:location] == 'Casino'
-      new_gold = Random.rand(-50..50)
+      new_gold = rand(-50..50)
       session[:gold] += new_gold
     end
 
